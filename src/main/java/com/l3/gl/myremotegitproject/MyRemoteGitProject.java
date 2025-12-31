23947218575f4902ab5d47f751e4e1605c6e3223
Yacine Zeghilet <zeghiletyacine12@gmail.com>
2025-12-31 18:52:41 +0100

    Initial commit: Setup Maven project structure.

---
+++ src/main/java/com/l3/gl/myremotegitproject/MyRemoteGitProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.l3.gl.myremotegitproject;
+
+/**
+ *
+ * @author Yacine
+ */
+public class MyRemoteGitProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
